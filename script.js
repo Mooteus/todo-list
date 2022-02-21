e62d59023e549dd574e6dcd7dc9a15f9f60ccbfb
Mooteus <moteus10@gmail.com>
2022-02-21 14:54:36 -0300

    verify input string

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function createTask() {
   const taskList = document.getElementById('lista-tarefas');
   const textTask = document.getElementById('texto-tarefa').value;
-  const task = document.createElement('li');
 
-  task.innerText = textTask;
-  taskList.appendChild(task);
+  if (textTask.trim() !== '') {
+    const task = document.createElement('li');
+    task.classList.add('task');
 
-  document.getElementById('texto-tarefa').value = '';
+    task.innerText = textTask;
+    taskList.appendChild(task);
+
+    document.getElementById('texto-tarefa').value = '';
+  }
 }
 
 const buttonTask = document.getElementById('criar-tarefa');
 buttonTask.addEventListener('click', createTask);
+
+const listClick = document.getElementsByClassName('');
