b23299e48041fef415e3d750d21a458cad5ef2bd
Mooteus <moteus10@gmail.com>
2022-02-21 14:45:39 -0300

    add text to list | clear input value

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function createTask() {
   const taskList = document.getElementById('lista-tarefas');
+  const textTask = document.getElementById('texto-tarefa').value;
   const task = document.createElement('li');
+
+  task.innerText = textTask;
   taskList.appendChild(task);
+
+  document.getElementById('texto-tarefa').value = '';
 }
 
 const buttonTask = document.getElementById('criar-tarefa');
